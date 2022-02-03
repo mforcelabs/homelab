a538446386779af37f5b85284eb805ac49304f75
mforce <mforce22@outlook.com>
2022-02-03 14:04:50 -0800

    mforcelabs

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "mforcelabs"
 
     workspaces {
       name = "homelab-external"
